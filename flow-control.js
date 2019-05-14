c86cea830fc4330f4ab06afe788f6582ce9f8092
Alex East <alexeastltd@gmail.com>
2019-05-14 03:08:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,5 +24,26 @@ return age >= 13 && age <= 19? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {}
-
+var age ="13"
+swith(age) {
+  case "14":
+  console.log("You are a teenager");
+  break;
+  case "15":
+  console.log("You are a teenager");
+  break;
+  case "16":
+  console.log("You are a teenager");
+  break;
+  case "17":
+  console.log("You are a teenager");
+  break;
+  case "18":
+  console.log("You are a teenager");
+  break;
+  case "19":
+  console.log("You are a teenager");
+  break;
+  default:
+  console.log("You have an age")
 }
